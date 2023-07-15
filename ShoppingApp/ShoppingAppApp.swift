d9e4206ca44d6f19ea6ad51f87b3bd9e772615de
exepr0gaming <exehomework@gmail.com>
2023-07-15 18:26:08 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Admin on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
